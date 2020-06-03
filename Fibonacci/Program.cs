6cb2a789295ff64fc54635c5c94539e5b5e78d6c
mrzepeckii <m.rzepaa@gmail.com>
2020-06-03 15:58:27 +0200

    Add project files.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type the number of Fibonacci elements ");
+            Int32.TryParse(Console.ReadLine(), out int n);
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(Fibo(i));
+            }
+        }
+
+        public static int Fibo(int n)
+        {
+            if (n == 0) return 0;
+            if (n < 3) return 1;
+            else return Fibo(n - 2) + Fibo(n - 1);
+        }
+    }
+}
